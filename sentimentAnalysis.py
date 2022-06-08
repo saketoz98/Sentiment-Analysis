a64ed446874085f7b14d3cddbd6cf097e709d5ad
Saket Ozarkar <saketozarkar@Sakets-Air.lan>
2022-06-07 21:32:41 -0700

    Modified code to follow PEP8 guidelines

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -1,11 +1,13 @@
-from visualization import *
-from properties import NEWS_ARTICLE_HOST
+import json
+
 from bs4 import BeautifulSoup
 from google.cloud import language_v1
 from tqdm import tqdm
 import requests
-import json
-  
+
+from visualization import *
+from properties import NEWS_ARTICLE_HOST
+
 def getSentimentOfDocument(content):
     client = language_v1.LanguageServiceClient()
     document = language_v1.Document(
@@ -18,18 +20,18 @@ def getSentimentOfDocument(content):
 
 def categorizeSentimentResult(sentiment):
     if sentiment.score >= 0.3:
-        return 'Positive'
+        return "Positive"
     elif sentiment.score <= -0.3:
-        return 'Negative'
+        return "Negative"
     else:
-        return 'Neutral'
+        return "Neutral"
 
 
 if __name__ == "__main__":
 
-    html_text = requests.get('https://www.aljazeera.com/where/mozambique/').text
-    soup = BeautifulSoup(html_text, 'lxml')
-    articles = soup.find_all("a", class_="u-clickable-card__link")
+    html_text = requests.get("https://www.aljazeera.com/where/mozambique/").text
+    parsedHtml = BeautifulSoup(html_text, "lxml")
+    articles = parsedHtml.find_all("a", class_="u-clickable-card__link")
 
     result = []
 
@@ -37,8 +39,10 @@ if __name__ == "__main__":
         articleLink = article["href"]
         url = NEWS_ARTICLE_HOST + articleLink
         articleHtmlText = requests.get(url).text
-        articleParsedHtml = BeautifulSoup(articleHtmlText, 'lxml')
-        articleContent = articleParsedHtml.find("div", class_="wysiwyg wysiwyg--all-content css-1ck9wyi")
+        articleParsedHtml = BeautifulSoup(articleHtmlText, "lxml")
+        articleContent = articleParsedHtml.find(
+                            "div", 
+                            class_="wysiwyg wysiwyg--all-content css-1ck9wyi")
         if articleContent is not None:
             text = ""
             paragraphs = articleContent.find_all("p")
@@ -46,18 +50,18 @@ if __name__ == "__main__":
                 for para in tqdm(paragraphs, desc="Extract Content of Paragraphs from Articles"):
                     text += para.getText()
             articleJson = dict()
-            articleJson['url'] = url
-            articleJson['content'] = text
+            articleJson["url"] = url
+            articleJson["content"] = text
 
             predictedSentiment = getSentimentOfDocument(text)
             sentimentCategoricalValue = categorizeSentimentResult(predictedSentiment)
-            articleJson['sentimentScore'] = predictedSentiment.score
-            articleJson['overallSentiment'] = sentimentCategoricalValue
+            articleJson["sentimentScore"] = predictedSentiment.score
+            articleJson["overallSentiment"] = sentimentCategoricalValue
             result.append(articleJson)
     
     visualizeResults(result)
     
-    with open('articles.json', 'w') as f:
+    with open("articles.json", "w") as f:
         json.dump(result, f, indent=2)
 
 
